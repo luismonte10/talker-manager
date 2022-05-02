f2bc7fcbdd4834752408c195d735c9d0bce59d5a
Luis Monte <luismonte.l560@gmail.com>
2022-05-02 11:16:34 -0300

    1 - Crie o endpoint GET /talker

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+const filePath = './talker.json';
+
+app.get('/talker', async (_req, res) => {
+  const response = await fs.readFile(filePath);
+  const data = JSON.parse(response);
+  if (data === undefined) return res.status(HTTP_OK_STATUS).json([]);
+  return res.status(HTTP_OK_STATUS).json(data);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
