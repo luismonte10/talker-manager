d8d5019fb17c0ee8450a2bcf4f7b673b6d59f49d
Luis Monte <luismonte.l560@gmail.com>
2022-05-02 14:40:43 -0300

    2 - Crie o endpoint GET /talker/:id

--- index.js
+++ index.js
@@ -6,15 +6,33 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 const PORT = '3000';
 
 const filePath = './talker.json';
 
-app.get('/talker', async (_req, res) => {
+const getTalkersList = async () => {
   const response = await fs.readFile(filePath);
   const data = JSON.parse(response);
-  if (data === undefined) return res.status(HTTP_OK_STATUS).json([]);
-  return res.status(HTTP_OK_STATUS).json(data);
+  return data;
+};
+
+// requisito 01
+app.get('/talker', async (_req, res) => {
+  const allTalkersList = await getTalkersList();
+  if (allTalkersList === undefined) return res.status(HTTP_OK_STATUS).json([]);
+  return res.status(HTTP_OK_STATUS).json(allTalkersList);
+});
+
+// requisito 02
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const allTalkersList = await getTalkersList();
+  const talkerById = allTalkersList.find((talker) => Number(talker.id) === Number(id));
+  if (talkerById === undefined) {
+    return res.status(HTTP_NOT_FOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(HTTP_OK_STATUS).json(talkerById);
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
