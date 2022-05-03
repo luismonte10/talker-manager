d7f204b1e8dc7d4c062811ff7d38a0622d32a49f
Luis Monte <luismonte.l560@gmail.com>
2022-05-03 19:41:45 -0300

    3 e 4 - Crie e adiciona as validações ao endpoint POST /login

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs/promises');
+const { emailValidation, passwordValidation } = require('./middlewares/loginMiddlewares');
 
 const app = express();
 app.use(bodyParser.json());
 
+const PORT = '3000';
+
+// status
 const HTTP_OK_STATUS = 200;
 const HTTP_NOT_FOUND_STATUS = 404;
-const PORT = '3000';
 
 const filePath = './talker.json';
 
+// pegar a lista completa
 const getTalkersList = async () => {
   const response = await fs.readFile(filePath);
   const data = JSON.parse(response);
   return data;
+};
+
+// gera um token aleatório 
+const createToken = () => {
+  let token = '';
+  for (let i = 1; i <= 16; i += 1) {
+      token += (Math.floor(Math.random() * 10));
+  }
+
+  return token;
 };
 
 // requisito 01
@@ -28,11 +42,15 @@ app.get('/talker', async (_req, res) => {
 app.get('/talker/:id', async (req, res) => {
   const { id } = req.params;
   const allTalkersList = await getTalkersList();
-  const talkerById = allTalkersList.find((talker) => Number(talker.id) === Number(id));
+  const talkerById = allTalkersList.find((talker) => talker.id === Number(id));
   if (talkerById === undefined) {
     return res.status(HTTP_NOT_FOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
   }
   return res.status(HTTP_OK_STATUS).json(talkerById);
+});
+
+app.post('/login', emailValidation, passwordValidation, (req, res) => {
+  res.status(HTTP_OK_STATUS).json({ token: createToken() });
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
